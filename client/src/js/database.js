67b1ebb948a345976d7e3e697ef9f71307abcb79
codemodeactivate <jeff.zenko@gmail.com>
2023-07-09 10:55:31 -0400

    database.js update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    await tx.store.put({ content });
+    await tx.done;
+  } catch (error) {
+    console.error('putDb failed:', error);
+  }
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    return await store.getAll();
+  } catch (error) {
+    console.error('getDb failed:', error);
+  }
+}
+
+
 
 initdb();
