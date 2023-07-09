d81fbe631fc5eca475883959b7a9559480bd39b5
codemodeactivate <jeff.zenko@gmail.com>
2023-07-09 10:58:53 -0400

    install.js update

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,30 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('👍', 'beforeinstallprompt', event); //thumbs up let's go
+    event.preventDefault();
+    deferredPrompt = event;
+    butInstall.style.display = 'block';
+
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    butInstall.style.display = 'none';
+    deferredPrompt.prompt();
+    const choiceResult = await deferredPrompt.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+    } else {
+        console.log('User dismissed the install prompt');
+    }
+    deferredPrompt = null;
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event); //success
+
+});
